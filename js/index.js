10938b30afd4ff6dbddd078508653f131d0f2e9c
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-17 09:52:37 +0430

    adding class

--- js/index.js
+++ js/index.js
@@ -4,7 +4,32 @@ const errMsg = document.getElementById("err-msg");
 
 linkForm.addEventListener("submit", formSubmit);
 
+// Validate a URL
+function validURL(str) {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+      "((\\d{1,3}\\.){3}\\d{1,3}))" +
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+      "(\\?[;&a-z\\d%_.~+=-]*)?" +
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return !!pattern.test(str);
+}
+
 function formSubmit(e) {
   e.preventDefault();
-  console.log(123);
+
+  if (input.value === "") {
+    errMsg.innerHTML = "Please enter something";
+    input.classList.add("border-red");
+  } else if (!validURL(input.value)) {
+    errMsg.innerHTML = "Please enter a valid URL";
+    input.classList.add("border-red");
+  } else {
+    errMsg.innerHTML = "";
+    input.classList.remove("border-red");
+    alert("Success");
+  }
 }
