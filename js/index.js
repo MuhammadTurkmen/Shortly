6c050d12740223067dd2d2651ac9c77fcf49bba9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-17 17:46:54 +0430

    adding js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 const input = document.getElementById("link-input");
 const linkForm = document.getElementById("link-form");
 const errMsg = document.getElementById("err-msg");
+
+linkForm.addEventListener("submit", formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault();
+  console.log(123);
+}
